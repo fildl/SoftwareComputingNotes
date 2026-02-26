f862bdfb99821e8cd48f3d45bd84694231bbd4bf
filo <filippodiludovico@icloud.com>
2026-02-26 19:04:41 +0100

    refactor: Improve LaTeX conversion script and reformat notebook headings for better presentation.

--- notes/convert_notes.sh
+++ notes/convert_notes.sh
@@ -28,13 +28,24 @@ if not tex_files:
 preamble = []
 with open(tex_files[0], 'r', encoding='utf-8') as f:
     for line in f:
-        preamble.append(line)
+        if r'\title{' in line:
+            preamble.append(r'    \title{Software and Computing for Applied Physics Notes}' + '\n')
+            preamble.append(r'    \author{Filippo Di Ludovico}' + '\n')
+            preamble.append(r'    \date{A.Y. 2025/26}' + '\n')
+        else:
+            preamble.append(line)
         if r'\begin{document}' in line:
+            # Inseriamo prima di document un override per i link
+            preamble.insert(-1, r'    \hypersetup{linkcolor=black, urlcolor=black, citecolor=black}' + '\n')
             break
 
 with open('master.tex', 'w', encoding='utf-8') as out:
     for line in preamble:
         out.write(line)
+        
+    out.write(r'    \maketitle' + '\n')
+    out.write(r'    \tableofcontents' + '\n')
+    out.write(r'    \newpage' + '\n')
     
     for tex in tex_files:
         out.write("\n% --- " + tex + " ---\n")
@@ -44,7 +55,7 @@ with open('master.tex', 'w', encoding='utf-8') as out:
                 if r'\end{document}' in line:
                     break
                 if body:
-                    out.write(line)
+                        out.write(line)
                 if r'\begin{document}' in line:
                     body = True
         out.write("\n\\newpage\n")
@@ -58,8 +69,14 @@ if [ -f "master.tex" ]; then
     # Double pass for indices / references (optional but recommended for LaTeX)
     pdflatex -interaction=nonstopmode master.tex
     
-    # Clean up
+    # Clean up temporary LaTeX files
     rm -f *.aux *.log *.out
+    
+    # Clean up intermediate individual .tex files (keeping only their generated content in master.tex)
+    for file in "${ipynb_files[@]}"; do
+        rm -f "${file%.*}.tex"
+    done
+    
     echo "============================="
     echo "Done! Created master.pdf"
     echo "============================="
